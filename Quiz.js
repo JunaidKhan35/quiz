20d0a6cc9d6c8235637eca2a12defb6838896641
JunaidKhan35 <80576517+JunaidKhan35@users.noreply.github.com>
2021-08-03 11:14:51 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,15 @@
+class Quiz{
+    constructor(){
+        this.input = createInput("Name")
+        this.button = createButton("Play")
+        
+    }
+
+    play(){
+        //this.input.hide()
+        //this.button.hide()
+        if(allContestant !== undefined){
+            
+        }
+    }
+}
